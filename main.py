6db520e3cc9cb0e28823ba7416dffbe514b624bb
SaraNofa <saranoofa2@gmail.com>
2026-06-03 12:02:43 +0800

    Create new user text file

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 with open('sample.txt') as file:
     content = file.read()
     
+with open('user_file.txt', 'w') as file:
+    file.write('User content')
+    
 print("File content: ")
 print(content)
 print("Done")
